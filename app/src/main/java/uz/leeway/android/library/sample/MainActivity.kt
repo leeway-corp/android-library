3c4024e32a9f6cac2025a0fd20873a4171616f4b
LeeWay <leeway@gmail.com>
2021-01-06 23:05:39 +0500

    lollipinx

--- app/src/main/java/uz/leeway/android/library/sample/MainActivity.kt
+++ app/src/main/java/uz/leeway/android/library/sample/MainActivity.kt
@@ -1,13 +1,20 @@
 package uz.leeway.android.library.sample
 
 import android.os.Bundle
-import uz.leeway.android.lib.lollipinx.PinCompatActivity
+import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
+import uz.leeway.android.library.sample.databinding.ActivityTestBinding
 
-class MainActivity : PinCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_test) {
+
+    private val viewBinding: ActivityTestBinding by viewBinding()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewBinding.hello.text = "Hello world333"
+        viewBinding.root.postDelayed({
+            viewBinding.message = "Tes messsage"
+        }, 3000)
     }
 
-    override fun getLayoutRes(): Int = R.layout.activity_main
 }
